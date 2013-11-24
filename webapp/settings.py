50a8d2c0d7cb74d2c951fa013d3acb90db55d229
James Pennell <pennell.james@gmail.com>
2013-11-24 13:59:51 -0500

    Add facebook login

--- webapp/settings.py
+++ webapp/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-SECRET_KEY = 't3c12e%g%i5$ehe40i*+ou)ycn2$afh@!)v2a)e@n(26vd81ef'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = True
 TEMPLATE_DEBUG = True
@@ -19,11 +19,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django.contrib.messages',
     'south',
     'debug_toolbar',
     'bootstrap3',
+    'social.apps.django_app.default',
     'webapp.apps.exchange',
 )
 
@@ -39,7 +39,20 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social.apps.django_app.middleware.SocialAuthExceptionMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'social.apps.django_app.context_processors.backends',
+    'social.apps.django_app.context_processors.login_redirect',
 )
 
 ROOT_URLCONF = 'webapp.urls'
@@ -48,6 +61,35 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = { }
 DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
+
+AUTHENTICATION_BACKENDS = (
+    'social.apps.django_app.utils.BackendWrapper',
+    'social.backends.facebook.FacebookOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.social_auth.associate_by_email',
+    'social.pipeline.user.get_username',
+    'social.pipeline.user.create_user',
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+)
+
+SOCIAL_AUTH_FACEBOOK_KEY = os.environ['SOCIAL_AUTH_FACEBOOK_KEY']
+SOCIAL_AUTH_FACEBOOK_SECRET = os.environ['SOCIAL_AUTH_FACEBOOK_SECRET']
+
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'locale': 'en_US'}
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_ERROR_URL = '/login/'
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 from django.contrib import admin
 
 admin.autodiscover()
@@ -8,6 +8,10 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    url('', include('social.apps.django_app.urls', namespace='social')),
+
     url(r'^$', RedirectView.as_view(url='/exchange/'), name='home'),
+    url(r'^login/$', TemplateView.as_view(template_name='accounts/login.html'), name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^exchange/', include('webapp.apps.exchange.urls')),
 )
